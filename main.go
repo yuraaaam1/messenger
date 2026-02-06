4d5715f9dd122d4ab26756843fd7916e798ec050
yuriy.m20031 <yuriy.m20031@gmail.com>
2026-02-06 12:03:33 +0000

    mock-server-frontend

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import(
+	"net/http"
+)
 
+func main() {
+	fs := http.FileServer(http.Dir("./frontend")
+	http.Handle("/", fs)
+
+	http.HandleFunc("/api/messages", func(w http.ResponseWriter, r *http.Request){
+		messages := []map[string]string{
+			{"user:": "Alice", "text": "Привет!"},
+			{"user:": "Bob", "text": "Привет, как дела?"},
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(messages)
+	})
+
+	log.Println("Запуск сервера на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
